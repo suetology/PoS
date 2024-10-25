38e4a31151c062f2dbb68a751676da1aaf4a985e
Eliseenko Vitas <vitas.eliseenko@mif.stud.vu.lt>
2024-10-25 16:15:51 +0300

    added cors rules

--- API/PoS.WebApi/Program.cs
+++ API/PoS.WebApi/Program.cs
@@ -5,6 +5,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options => 
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -35,6 +45,8 @@ app.MapGet("/weatherforecast", () =>
     })
     .WithName("GetWeatherForecast")
     .WithOpenApi();
+
+app.UseCors("AllowAll");
 
 app.Run();
 
