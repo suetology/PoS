56904ac3692120d54b8ed2e0bdfc5eef36918170
Patricija Katinaite <116752299+Patriciakat@users.noreply.github.com>
2024-11-02 20:49:57 +0200

    Business controller (#3)

---
+++ API/PoS.WebApi/Application/Services/Business/BusinessService.cs
@@ -0,0 +1,58 @@
+﻿namespace PoS.WebApi.Application.Services.Business;
+
+using Contracts;
+using Domain.Entities;
+using PoS.WebApi.Domain.Common;
+using Repositories;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public class BusinessService : IBusinessService
+{
+    private readonly IBusinessRepository _businessRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public BusinessService(IBusinessRepository businessRepository, IUnitOfWork unitOfWork)
+    {
+        _businessRepository = businessRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task CreateBusiness(BusinessDto businessDto)
+    {
+        var business = businessDto.ToDomain();
+
+        await _businessRepository.Create(business);
+        await _unitOfWork.SaveChanges();
+    }
+
+    public async Task<IEnumerable<Business>> GetAllBusiness()
+    {
+        return await _businessRepository.GetAll();
+    }
+
+    public async Task<Business> GetBusiness(Guid businessId)
+    {
+        return await _businessRepository.Get(businessId);
+    }
+
+    public async Task<bool> UpdateBusiness(Guid businessId, BusinessDto businessDto)
+    {
+        var existingBusiness = await _businessRepository.Get(businessId);
+
+        if (existingBusiness == null)
+        {
+            return false;
+        }
+
+        var updatedBusiness = businessDto.ToDomain();
+        existingBusiness.Update(updatedBusiness);
+
+        _businessRepository.Update(existingBusiness);
+        await _unitOfWork.SaveChanges();
+
+        return true;
+
+    }
+}
--- API/PoS.WebApi/Program.cs
+++ API/PoS.WebApi/Program.cs
@@ -4,7 +4,9 @@ using PoS.WebApi.Application.Repositories;
 using PoS.WebApi.Application.Services.ServiceCharge;
 using PoS.WebApi.Application.Services.Customer;
 using PoS.WebApi.Application.Services.Tax;
+using PoS.WebApi.Application.Services.Business;
 using PoS.WebApi.Domain.Common;
+using PoS.WebApi.Domain.Entities;
 using PoS.WebApi.Infrastructure.Persistence;
 using PoS.WebApi.Infrastructure.Repositories;
 using PoS.WebApi.Presentation.Extensions;
@@ -39,9 +41,13 @@ builder.Services.AddScoped<IServiceChargeRepository, ServiceChargeRepository>();
 builder.Services.AddScoped<IServiceChargeService, ServiceChargeService>();
 builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
 builder.Services.AddScoped<ICustomerService, CustomerService>();
+
 builder.Services.AddTransient<ITaxService, TaxService>();
 
 builder.Services.AddTransient<ITaxRepository, TaxRepository>();
+
+builder.Services.AddTransient<IBusinessService, BusinessService>();
+builder.Services.AddTransient<IBusinessRepository, BusinessRepository>();
 
 // Adding controllers
 builder.Services.AddControllers();
