9e744b41d33385901539c258d66c38429eea6495
Patricija <patricija.katinaite@gmail.com>
2024-11-03 00:47:58 +0200

    User controller implementation

---
+++ API/PoS.WebApi/Presentation/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PoS.WebApi.Application.Services.User;
+using PoS.WebApi.Application.Services.User.Contracts;
+
+namespace PoS.WebApi.Presentation.Controllers;
+
+[ApiController]
+[Route("user")]
+public class UserController :ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllUsers()
+    {
+        var allUsers = await _userService.GetAllUsers();
+
+        return Ok(allUsers);
+    }
+
+    [HttpPost(Name = nameof(CreateUser))]
+    public async Task<IActionResult> CreateUser([FromBody] UserDto userDto)
+    {
+        await _userService.CreateUser(userDto);
+
+        return NoContent();
+    }
+}
