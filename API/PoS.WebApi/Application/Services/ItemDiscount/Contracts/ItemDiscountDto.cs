ac13a726a91d07180a74b25027fee78a78fe8868
Patricija <patricija.katinaite@gmail.com>
2024-11-11 21:21:30 +0200

    ItemDiscount implementation, so now you can assign discount to a specific item

---
+++ API/PoS.WebApi/Application/Services/ItemDiscount/Contracts/ItemDiscountDto.cs
@@ -0,0 +1,9 @@
+﻿namespace PoS.WebApi.Application.Services.ItemDiscount.Contracts;
+
+public class ItemDiscountDto
+{
+    public Guid ItemId { get; set; }
+    public string ItemName { get; set; }
+    public Guid DiscountId { get; set; }
+    public string DiscountName { get; set; }
+}
