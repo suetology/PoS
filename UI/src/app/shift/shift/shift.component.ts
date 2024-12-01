74f61adc4be573587aea42a170abe06004d3ba62
Patricija <patricija.katinaite@gmail.com>
2024-12-01 20:16:53 +0200

    routing setup

---
+++ UI/src/app/shift/shift/shift.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shift',
+  standalone: true,
+  imports: [],
+  templateUrl: './shift.component.html',
+  styleUrl: './shift.component.css'
+})
+export class ShiftComponent {
+
+}
