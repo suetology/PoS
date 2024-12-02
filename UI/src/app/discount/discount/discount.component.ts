534aa2f11a6ebdc468fa44b7a0b1210e6f51e81e
Patricija <patricija.katinaite@gmail.com>
2024-12-02 12:20:26 +0200

    fetched discount

--- UI/src/app/discount/discount/discount.component.ts
+++ UI/src/app/discount/discount/discount.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { DiscountService } from '../../services/discount.service';
+import { Observable } from 'rxjs';
+import { Discount } from '../../types';
+import { AsyncPipe, CommonModule, DatePipe } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AddDiscountComponent } from '../add-discount/add-discount.component';
 
 @Component({
   selector: 'app-discount',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, AsyncPipe, DatePipe, RouterModule, AddDiscountComponent],
   templateUrl: './discount.component.html',
   styleUrl: './discount.component.css'
 })
 export class DiscountComponent {
 
+  discounts$: Observable<Discount[]>;
+
+  constructor(private discountService : DiscountService){
+    this.discounts$ = this.discountService.getDiscounts();
+  }
 }
